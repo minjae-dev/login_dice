3cc364d64656ff079639a6607231a4b81d5d1b60
minjamie <minjae2246@gamil.com>
2025-10-19 22:34:46 -0700

    login Form/button component

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,12 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class LogIn extends StatelessWidget {
+class LogIn extends StatefulWidget {
+  @override
+  State<LogIn> createState() => _LogInState();
+}
+
+class _LogInState extends State<LogIn> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,18 +26,81 @@ class LogIn extends StatelessWidget {
         title: Text('Log in'),
         backgroundColor: Colors.redAccent,
         centerTitle: true,
-        leading: IconButton(
-            icon: Icon(Icons.menu),
-            onPressed: (){}
-        ),
+        leading: IconButton(icon: Icon(Icons.menu), onPressed: () {}),
         actions: <Widget>[
-          IconButton(
-              icon: Icon(Icons.search),
-              onPressed: (){}
-          )
+          IconButton(icon: Icon(Icons.search), onPressed: () {})
         ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(top: 50.0),
+              child: Center(
+                child: Image(
+                  width: 200,
+                  height: 200,
+                  image: AssetImage(
+                    'image/chef.gif',
+                  ),
+                ),
+              ),
+            ),
+            Form(
+              child: Theme(
+                data: ThemeData(
+                  primaryColor: Colors.teal,
+                  inputDecorationTheme: InputDecorationTheme(
+                    labelStyle: TextStyle(
+                      color: Colors.teal,
+                      fontSize: 15.0,
+                    ),
+                  ),
+                ),
+                child: Container(
+                  child: Padding(
+                    padding: const EdgeInsets.all(40.0),
+                    child: Column(
+                      children: [
+                        TextFormField(
+                          decoration: InputDecoration(
+                            labelText: 'Enter email',
+                          ),
+                          keyboardType: TextInputType.emailAddress,
+                        ),
+                        TextFormField(
+                          decoration: InputDecoration(
+                            labelText: 'Enter password',
+                          ),
+                          keyboardType: TextInputType.text,
+                          obscureText: true,
+                        ),
+                        SizedBox(
+                          height: 40.0,
+                        ),
+                        SizedBox(
+                          width: 100.0,
+                          height: 50.0,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.orangeAccent,
+                            ),
+                            onPressed: () {},
+                            child: Icon(
+                              Icons.arrow_forward,
+                              color: Colors.white,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ), // Replace null with a valid widget
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
-
